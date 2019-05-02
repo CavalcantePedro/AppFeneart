46bdd70c8f125452adddd92fdf7a122840664689
tucoff <baracho.arturraffael@gmail.com>
2019-05-02 10:01:25 -0300

    AR_Base

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton : MonoBehaviour
+{
+    private static Singleton instance;
+
+    public static Singleton GetInstance{
+        get{
+            if(instance == null){
+                instance = GameObject.FindObjectOfType<Singleton>();
+            }
+
+            return instance;
+        }
+    }
+}
