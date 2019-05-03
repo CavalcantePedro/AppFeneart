75499ae21b0cd992bd64944a1ac223a512d5f35f
Aluno <aluno@naverecife.local>
2019-05-03 08:00:33 -0300

    Fade with Scene Transitions - Done!

---
+++ Assets/Scripts - Rabih/SceneChanger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    private float delay;
+    [SerializeField] private Animator anim;
+    private string sceneName;
+    // Start is called before the first frame update
+    void Start()
+    {
+     delay = 1f;
+    }
+
+   public void ChangeScene(string name)
+   {
+       sceneName = name;
+       StartCoroutine("Transition");
+   }
+
+   IEnumerator Transition()
+   {
+       anim.SetTrigger("changingScene");
+       yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(sceneName);   
+
+   }
+
+}
